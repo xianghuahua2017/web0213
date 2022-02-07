52bd6997a519d4a629bd72f2e3cb5978f35ed79a
wxh <wxh1058@163.com>
2022-02-07 20:17:56 +0800

    更新readme.txt

--- 03routerParams/webpack.config.js
+++ 03routerParams/webpack.config.js
@@ -6,39 +6,39 @@ module.exports={
     output:{
             path:__dirname+"/build",
             filename:"bundle.js"
-        },
-        module:{
-            rules:[
-                {
-                    test : /\.json$/,
-                    use:"babel-loader"
-                },
-                {
-                    test : /\.js$/,
-                    use:"babel-loader"
-                },
-                {
-                    test:/\.css$/,
-                    use:["style-loader","css-loader"]
-                },
-                {
-                    test:/\.less$/,
-                    use:["style-loader","css-loader","less-loader"]
-                },
-                {
-                    test:/\.(jpg|png|gif|jpeg)$/,
-                    use:"url-loader?limit=8129"
-                }
+    },
+    module:{
+        rules:[
+            {
+                test : /\.json$/,
+                use:"babel-loader"
+            },
+            {
+                test : /\.js$/,
+                use:"babel-loader"
+            },
+            {
+                test:/\.css$/,
+                use:["style-loader","css-loader"]
+            },
+            {
+                test:/\.less$/,
+                use:["style-loader","css-loader","less-loader"]
+            },
+            {
+                test:/\.(jpg|png|gif|jpeg)$/,
+                use:"url-loader?limit=8129"
+            }
 
-            ]
-        },
-        plugins:[
-            new openBrowser({
-                url:"http://localhost:8080"
-            }),
-            new htmlPlugin({
-                template:__dirname+'/src/app/index.tmpl.html'
-            })
         ]
+    },
+    plugins:[
+        new openBrowser({
+            url:"http://localhost:8080"
+        }),
+        new htmlPlugin({
+            template:__dirname+'/src/app/index.tmpl.html'
+        })
+    ]
 
 }
